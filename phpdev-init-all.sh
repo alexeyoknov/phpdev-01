b74308a26cefb400c97b83069a6b38b851f66704
Alexey <pitrider@mail.ru>
2022-01-30 16:34:33 +0300

    Release

--- phpdev-init-all.sh
+++ phpdev-init-all.sh
@@ -76,3 +76,31 @@ sudo apt install -y code
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+name=`whoami`
+
+echo $name
+
+echo "Generating ssh key for git"
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+	ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+fi
+
+echo "Export this SSH PUBLIC KEY to git"
+cat ~/.ssh/id_rsa.pub
+
+# Git config
+echo "Git global config"
+git config --global user.name "Alexey Oknov"
+git config --global user.email "pitrider@mail.ru"
+
+echo "Adding pma & dev01 to /etc/hosts"
+b=`fgrep pma.my /etc/hosts`
+if [ -z "$b" ]; then 
+	echo "127.0.0.1 pma.my" | sudo tee -a /etc/hosts
+fi
+
+b=`fgrep dev01.my /etc/hosts`
+if [ -z "$b" ]; then 
+	echo "127.0.0.1 dev01.my" | sudo tee -a /etc/hosts
+fi
+
